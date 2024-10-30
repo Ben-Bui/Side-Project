6025894bc4ceafe4094e4c8cd79ffaf35ab42e2e
Ben bui <btb4516@g.rit.edu>
2024-10-30 12:05:51 +0700

    Better work flow/ tsconfig

---
+++ TypeScript Tutorial/src/sandbox.ts
@@ -0,0 +1,9 @@
+// tsc --init
+console.log('test');
+//another test
+//another test
+//Step 1. tsc--init
+//Step 2. Go into the files changes rootDir "./", choose the path .ts file is in
+//Step 3. outDir "./", change the path output to where your.js file is in
+//Step 4. tsc -w in the terminal
+//test
