5414dab81c1cf8ad12bf8c455c1f793244ebaa42
Ben bui <btb4516@g.rit.edu>
2024-10-30 10:53:05 +0700

    array object

--- TypeScript Tutorial/sandbox.ts
+++ TypeScript Tutorial/sandbox.ts
@@ -1,28 +1,36 @@
 //array
-var names = ['luigi', 'mario', 'yoshi'];
+let names = ['luigi', 'mario', 'yoshi'];
+
 names.push('toad');
 // names.push(3);
 // names[0] = 3;
-var numbers = [10, 20, 30, 40];
+
+let numbers = [10, 20, 30, 40]
+
 numbers.push(25);
 // numbers.push('shaun');
 // numbers[1] = 'shaun';
-var mixed = ['ken', 4, 'chun-li', 8, 9];
+
+let mixed = ['ken', 4, 'chun-li', 8, 9];
+
 mixed.push('ryu');
 mixed.push(10);
 mixed[0] = 3;
+
 //object
-var ninja = {
+let ninja = {
     name: 'mario',
     belt: 'black',
     age: 30
 };
+
 ninja.age = 40;
 ninja.name = 'ryu';
 // ninja.age = '30'
 // ninja.skills = ['fighting', 'sneaking']
+
 ninja = {
     name: 'yoshi',
     belt: 'orange',
     age: 40,
-};
+}
