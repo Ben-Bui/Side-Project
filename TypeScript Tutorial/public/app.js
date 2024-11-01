10cd0c643c301fbf5d0be72d161cb86c9f63da74
Ben bui <btb4516@g.rit.edu>
2024-11-01 09:55:50 +0700

    Public, Private & Readonly

--- TypeScript Tutorial/public/app.js
+++ TypeScript Tutorial/public/app.js
@@ -1,13 +1,16 @@
 "use strict";
 //class
 class Invoice {
-    constructor(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    // readonly client: string;
+    // private details: string;
+    // public amount: number;
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     format() {
-        return '${this.client} owes $${this.amount} for ${this.details}';
+        return `${this.client} owes ${this.amount} for ${this.details}`;
     }
 }
 const invOne = new Invoice('mario', 'work on the mario website', 250);
@@ -15,10 +18,9 @@ const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
-invOne.client = 'yoshi';
-invTwo.amount = 400;
-console.log(invOne, invTwo);
-console.log(invoices);
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format());
+});
 const form = document.querySelector('.new-item-form');
 //input
 const type = document.querySelector('#type');
--- TypeScript Tutorial/src/app.ts
+++ TypeScript Tutorial/src/app.ts
@@ -1,17 +1,17 @@
 //class
 class Invoice{
-    client: string;
-    details: string;
-    amount: number;
+    // readonly client: string;
+    // private details: string;
+    // public amount: number;
 
-    constructor(c: string, d: string, a: number){
-        this.client = c;
-        this.details = d;
-        this.amount = a;
-    }
+    constructor(
+        readonly client: string,
+        private details: string,
+        public amount: number,
+    ){}
 
     format(){
-        return '${this.client} owes $${this.amount} for ${this.details}';
+        return `${this.client} owes ${this.amount} for ${this.details}`;
     }
 }
 
@@ -22,12 +22,11 @@ let invoices: Invoice[] = [];
 invoices.push(invOne);
 invoices.push(invTwo);
 
-invOne.client = 'yoshi';
-invTwo.amount = 400;
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format());
+});
 
-console.log(invOne,invTwo);
 
-console.log(invoices);
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
