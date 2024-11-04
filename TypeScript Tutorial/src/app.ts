8239332d54a472f63e1e19a0997237039f0dfe75
Ben bui <btb4516@g.rit.edu>
2024-11-04 15:08:46 +0700

    tuples

--- TypeScript Tutorial/src/app.ts
+++ TypeScript Tutorial/src/app.ts
@@ -18,38 +18,57 @@ const list = new ListTemplate(ul);
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
 
+    let values: [string, string, number];
+    values = [tofrom.value, details.value, amount.valueAsNumber]
+
     let doc: HasFormatter;
     if (type.value === 'invoice'){
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+        doc = new Invoice(...values)
     }else{
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
+        doc = new Payment(...values)
     }
     
     list.render(doc, type.value, 'end')
 
 });
 
-// Enums
-enum ResourceType {BOOK, AUTHOR, FILM, DIRECTOR, PERSON}
+//Tuples
 
-interface Resource<T> {
-    uid: number;
-    resourceType: ResourceType;
-    data: T;
-}
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi'];
 
-const docOne: Resource<object> = {
-    uid : 1,
-    resourceType: ResourceType.BOOK,
-    data: {title: ' name of the wind'}
-}
-const docTwo: Resource<object> = {
-    uid : 10,
-    resourceType: ResourceType.PERSON,
-    data: { name: 'yoshi'}
-}
+let tup: [string, number, boolean] = ['ryu', 25, true];
+tup[0] = 'ken';
+tup[1] = 30;
 
-console.log(docOne, docTwo);
+// let student: [string, number];
+// student: [12345, 'ken'];
+// student = ['chun-li', 223423];
+
+
+// // Enums
+// enum ResourceType {BOOK, AUTHOR, FILM, DIRECTOR, PERSON}
+
+// interface Resource<T> {
+//     uid: number;
+//     resourceType: ResourceType;
+//     data: T;
+// }
+
+// const docOne: Resource<object> = {
+//     uid : 1,
+//     resourceType: ResourceType.BOOK,
+//     data: {title: ' name of the wind'}
+// }
+// const docTwo: Resource<object> = {
+//     uid : 10,
+//     resourceType: ResourceType.PERSON,
+//     data: { name: 'yoshi'}
+// }
+
+// console.log(docOne, docTwo);
 // Generics
 // const addUID = <T extends {name: string}>(obj: T) => {
 //     let uid = Math.floor(Math.random() * 100);
